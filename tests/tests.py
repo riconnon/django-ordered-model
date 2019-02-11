28bcb16b3d7295a4d761dbc09627c4f2b75167b6
Chris Le Sueur <c.m.lesueur@gmail.com>
2019-02-11 11:52:04 +0000

    Add test to detect incorrect behaviour when changing an ordered_with_respect_to ForeignKey

--- tests/tests.py
+++ tests/tests.py
@@ -275,6 +275,11 @@ class OrderWithRespectToTests(TestCase):
             ],
         )
 
+    def test_change_respect_to(self):
+        self.q2_a2.question = self.q1_a1.question
+        self.q2_a2.save()
+        self.assertNotIn(self.q2_a2.order, [self.q1_a1.order, self.q1_a2.order])
+
 
 class CustomPKTest(TestCase):
     def setUp(self):
